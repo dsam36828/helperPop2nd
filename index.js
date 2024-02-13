306334c93b2f544dacb6cdda48c478f9e03507d4
dsam36828 <159868637+dsam36828@users.noreply.github.com>
2024-02-13 10:11:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,8 @@ const PORT = 3000 || process.env.PORT;
 
 // List of allowed frontend origins for CORS
 const allowedOrigins = [
-  "https://thaibasiljp.com",
-  "https://sakiinstant.shop",
-  "https://sakiinstant.shop/",
-  "http://sakiinstant.shop",
-  "http://sakiinstant.shop/",
-  "http://thaibasiljp.com",
-  "https://theteamania.shop",
-  "http://theteamania.shop",
-  "https://theteamania.shop/",
-  "http://theteamania.shop/",
+  "https://musubijp.com",
+  "http://musubijp.com",
   "http://127.0.0.1:5501",
   "http://127.0.0.1:5500",
 ];
